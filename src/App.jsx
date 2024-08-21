dac2027eb787dcca2ae327b3ce59a8a6e6801ef0
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2024-08-21 01:52:16 -0500

    Added dark and light mode toggle

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import FileUploader from './components/FileUploader';
 import EPUBViewer from './components/EPUBViewer';
 import './App.css';
+import { ModeToggle } from './components/mode-toggle';
+import { ThemeProvider } from './components/theme-provider';
 
 function App() {
   const [file, setFile] = useState(null);
@@ -12,12 +14,17 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1>EPUB Viewer</h1>
-      <FileUploader onFileSelect={setFile} />
-      {file && <button onClick={handleShowViewer}>Show EPUB Viewer</button>}
-      {file && showViewer && <EPUBViewer key={file.name} file={file} />}
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="App">
+        <div className="mode-toggle-container">
+          <ModeToggle />
+        </div>
+        <h1>EPUB Viewer</h1>
+        <FileUploader onFileSelect={setFile} />
+        {file && <button onClick={handleShowViewer}>Show EPUB Viewer</button>}
+        {file && showViewer && <EPUBViewer key={file.name} file={file} />}
+      </div>
+    </ThemeProvider>
   );
 }
 
